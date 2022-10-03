cf02d12b88a6ce9b96875bc176f952f180413692
Cam <21029087+csqrl@users.noreply.github.com>
2022-10-03 12:56:31 +0100

    Remove create util

--- src/FindUpAll.spec.lua
+++ src/FindUpAll.spec.lua
@@ -1,37 +1,11 @@
-local Create = require(script.Parent.Util.Create)
+--!strict
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local FindUpAll = require(script.Parent.FindUpAll)
 
-local TARGET = Create("Folder", {
-	Name = "__TEST_TARGET__",
-	Parent = script,
-})
-
-local START = Create("Folder", {
-	Name = "__START__",
-	Parent = Create("Folder", {
-		Parent = Create("Folder", {
-			Parent = TARGET,
-		}),
-	}),
-})
+local TestRoot = ReplicatedStorage.TestTarget:FindFirstChild("Thing1", true)
 
 return function()
-	local OBJECTS = {
-		MODULE = Create("ModuleScript", { Name = "SomeModule", Parent = TARGET }),
-		PART = Create("Part", { Name = "SomeModule", Parent = TARGET }),
-	}
-
 	it("should find the correct objects", function()
-		local objects = FindUpAll(START, "SomeModule")
-
-		expect(#objects).to.equal(2)
-		expect(table.find(objects, OBJECTS.MODULE)).to.be.ok()
-		expect(table.find(objects, OBJECTS.MODULE)).to.be.ok()
-	end)
-
-	afterAll(function()
-		for _, obj in pairs(OBJECTS) do
-			obj:Destroy()
-		end
+		expect(#FindUpAll(TestRoot, "SendMoney")).to.equal(2)
 	end)
 end
