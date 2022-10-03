5407cb93ee528a7091ba948e8362b8f759f69b24
Cam <21029087+csqrl@users.noreply.github.com>
2022-10-03 10:11:01 +0100

    Use aftman

--- .vscode/scripts/run-tests.ps1
+++ .vscode/scripts/run-tests.ps1
@@ -1,4 +1,8 @@
-foreman install
-rojo build .\test.project.json -o .\test-place.rbxlx
-run-in-roblox --place .\test-place.rbxlx --script .\test-runner.server.lua
-Remove-Item -Force .\test-place.rbxlx
+$FILE_NAME = ".\test-place.rbxl"
+$TEST_PROJ = ".\test.project.json"
+$TEST_RUNR = ".\test-runner.server.lua"
+
+aftman install
+rojo build "$TEST_PROJ" -o "$FILE_NAME"
+run-in-roblox --place "$FILE_NAME" --script "$TEST_RUNR"
+Remove-Item "$FILE_NAME" -Force -ErrorAction SilentlyContinue
